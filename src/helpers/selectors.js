3acb3ccff83c9e075e092fa265519cf4c3d96713
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-10-04 09:20:28 -0700

    Helper function to get Appointments for specific day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,41 +1,79 @@
-
 // const state_ = {
 //   days: [
 //     {
 //       id: 1,
 //       name: "Monday",
-//       appointments: [1, 2, 3]
+//       appointments: [1, 2, 3],
 //     },
 //     {
 //       id: 2,
 //       name: "Tuesday",
-//       appointments: [4, 5]
-//     }
+//       appointments: [4, 5],
+//     },
 //   ],
 //   appointments: {
-//     "1": { id: 1, time: "12pm", interview: null },
-//     "2": { id: 2, time: "1pm", interview: null },
-//     "3": {
+//     1: { id: 1, time: "12pm", interview: null },
+//     2: { id: 2, time: "1pm", interview: null },
+//     3: {
 //       id: 3,
 //       time: "2pm",
-//       interview: { student: "Archie Cohen", interviewer: 2 }
+//       interview: { student: "Archie Cohen", interviewer: 2 },
 //     },
-//     "4": { id: 4, time: "3pm", interview: null },
-//     "5": {
+//     4: { id: 4, time: "3pm", interview: null },
+//     5: {
 //       id: 5,
 //       time: "4pm",
-//       interview: { student: "Chad Takahashi", interviewer: 2 }
-//     }
-//   }
+//       interview: { student: "Chad Takahashi", interviewer: 2 },
+//     },
+//   },
+//   interviewers: {
+//     1: {
+//       id: 1,
+//       name: "Sylvia Palmer",
+//       avatar: "https://i.imgur.com/LpaY82x.png",
+//     },
+//     2: {
+//       id: 2,
+//       name: "Tori Malcolm",
+//       avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//     },
+//   },
 // };
 export function getAppointmentsForDay(state, day_) {
-  const result=[];
-  const dayObj = state.days.find((d)=> d.name === day_);
+  const result = [];
+  const dayObj = state.days.find((d) => d.name === day_);
   if (dayObj) {
-  for (let appId of dayObj.appointments) {
-    result.push(state.appointments[appId]);
-  } }
+    for (let appId of dayObj.appointments) {
+      result.push(state.appointments[appId]);
+    }
+  }
   return result;
-
 }
-//getAppointmentsForDay(state_, 'Wednesday');
+
+export function getInterview(state, interview) {
+  const newObj = {};
+  if (interview!==null) {
+    const id_ = interview.interviewer;
+    console.log(id_);
+    newObj.student = interview.student;
+    newObj.interviewer = state.interviewers[id_];
+    return newObj;
+  } else {
+  return null;
+  }
+}
+
+export function getInterviewersForDay(state, day_) {
+  const result = [];
+  const dayObj = state.days.find((d) => d.name === day_);
+  if (dayObj) {
+    for (let appId of dayObj.interviewers) {
+      result.push(state.interviewers[appId]);
+    }
+  }
+
+  return result;
+}
+
+
+//getInterview(state_, state_.appointments["3"].interview);
