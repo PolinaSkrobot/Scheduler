b05759a313855143ccef7d7c60407dfe29abe8e7
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-10-06 21:24:22 -0700

    updateSpots refactoring to get rid of bug in the websockets use

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,7 +12,8 @@ export default function useApplicationData() {
   });
   const setDay = (day) => setState({ ...state, day });
 
-  useEffect(() => {//api calls and setting new state with json data
+  useEffect(() => {
+    //api calls and setting new state with json data
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
@@ -27,52 +28,49 @@ export default function useApplicationData() {
     });
   }, []);
 
-  useEffect(()=> {//connection to the websocket server
+  useEffect(() => {
+    //connection to the websocket server
     const ws = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-        ws.addEventListener("open", ()=>{
-         // console.log("Connected!!!");
-        });
-        ws.onopen = function (event) {
-          // ws.send("ping");
-        };
-        ws.onmessage = function(event) {
-          const msg = JSON.parse(event.data);
-
-          if (msg.type === "SET_INTERVIEW") {
-            const newApp={...state.appointments[msg.id], interview: msg.interview };
-            const newApps={...state.appointments, [msg.id]: newApp};
-            updateSpots(newApps);            
-          }
-
-        };
-        return () => ws.close();
-  })
-
-  function updateSpots(appointments_) {
-    const objToPassToFunct = {//object to pass to function      
-      ...state,
-      appointments: appointments_,
+    ws.addEventListener("open", () => {
+      // console.log("Connected!!!");
+    });
+    ws.onopen = function (event) {
+      // ws.send("ping");
     };
-    const dailyAppointments = getAppointmentsForDay(//call hepler function to get app-s for the day
-      objToPassToFunct,
-      state.day
-    );
-    const filteredSpots = dailyAppointments.filter(//looking for nulls
-      (elem) => elem.interview === null
-    );
-    const newDays = [...state.days];//copy content of state.day
+    ws.onmessage = function (event) {
+      const msg = JSON.parse(event.data);
 
-    for (let i = 0; i < newDays.length; i++) {//looking for the day to change the num of available spots
-      if (newDays[i].name === state.day) {
-        newDays[i].spots = filteredSpots.length;
+      if (msg.type === "SET_INTERVIEW") {
+        const newApp = {
+          ...state.appointments[msg.id],
+          interview: msg.interview,
+        };
+        const newApps = { ...state.appointments, [msg.id]: newApp };
+        const newDays = updateSpots(msg.id, newApps);
+        setState((prev) => ({//setter to render the page
+          ...prev,
+          appointments: newApps,
+          days: newDays,
+        }));
+
       }
-    }
+    };
+    return () => ws.close();
+  });
 
-    setState((prev) => ({//setter to render the page
-      ...prev,
-      appointments: appointments_,
-      days: newDays
-    }));
+  function updateSpots(id, appointments_) {
+    return state.days.map((day) => {
+      let vacSpots = 0;
+      if (day.appointments.includes(id)) {
+        for (const app_id of day.appointments) {
+          if (appointments_[app_id].interview === null) {
+            vacSpots += 1;
+          }
+        }
+        day.spots = vacSpots;
+      }
+      return day;
+    });   
   }
 
   function bookInterview(id, interview) {
@@ -80,17 +78,22 @@ export default function useApplicationData() {
       ...state.appointments[id],
       interview: { ...interview },
     };
-    const appointments_ = {//new appointments obj with passed interview data
+    const appointments_ = { //new appointments obj with passed interview data
       ...state.appointments,
       [id]: appointment,
     };
 
-    return axios//request to the server
+    return axios //request to the server
       .put(`/api/appointments/${id}`, {
         interview,
       })
       .then(() => {
-        updateSpots(appointments_);
+        const newDays = updateSpots(id, appointments_);
+        setState((prev) => ({//setter to render the page
+          ...prev,
+          appointments: appointments_,
+          days: newDays,
+        }));
       });
   }
 
@@ -108,7 +111,12 @@ export default function useApplicationData() {
         interview_,
       })
       .then(() => {
-        updateSpots(appointments_);
+        const newDays = updateSpots(id, appointments_);
+        setState((prev) => ({//setter to render the page
+          ...prev,
+          appointments: appointments_,
+          days: newDays,
+        }));
       });
   }
 
